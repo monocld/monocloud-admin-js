ad4ebd1384e0f8f8e94d786c684044e9ae817a15
Sathyajith P S <sathyajithsps@gmail.com>
2022-07-20 11:45:15 +0530

    Updated Exceptions - v0.0.8

--- src/exceptions/not-found-exception.ts
+++ src/exceptions/not-found-exception.ts
@@ -1,1 +1,6 @@
-export class NotFoundException extends Error {}
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export class NotFoundException extends Error {
+  constructor(public readonly message: string, public readonly raw: any) {
+    super(message);
+  }
+}
